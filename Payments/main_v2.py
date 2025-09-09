9a335be69973a183963a620f583424068c6abb80
donits09 <98079068+donits09@users.noreply.github.com>
2025-09-09 11:24:18 +0800

    Gracefully handle missing fonts

--- Payments/main_v2.py
+++ Payments/main_v2.py
@@ -10,6 +10,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from common_paths import base_dir, data_dir, find_latest_csv
+from font_utils import safe_add_font
 
 # Example A: open the latest "appointments_" CSV automatically
 csv_path = find_latest_csv(prefix="appointments_")
@@ -20,19 +21,26 @@ if not csv_path:
     )
     raise SystemExit(1)
 
+novecento_font = "Helvetica"
+armata_font = "Helvetica"
+treb_bold_font = "Helvetica"
+treb_bolditalic_font = "Helvetica"
+treb_regular_font = "Helvetica"
+
+
 class PDF(FPDF):
     def header(self):
         self.image(str(base_dir() / 'Header.jpg'), 27.5, 0, 150, 35)
         self.ln(20)
-        self.set_font('Novecento Wide Demi Bold', 'B', 9)
+        self.set_font(novecento_font, 'B', 9)
         self.cell(0, 0, 'LIST OF APPOINTMENTS FOR ' + str(var_date), 'L')
         self.ln(4)
-        self.set_font('Armata Regular', '', 7)
+        self.set_font(armata_font, '', 7)
         self.cell(0, 0, 'Payments', 'L')
-        self.set_font('Armata Regular', '', 7)
+        self.set_font(armata_font, '', 7)
         self.cell(4, 0, 'Page ' + str(self.page_no()) + ' of {nb}', 0, 0, 'R')
         self.ln(2)
-        self.set_font('Trebuchet MS Bold', 'B', 7)
+        self.set_font(treb_bold_font, 'B', 7)
         self.set_fill_color(180)
         self.cell(0, 5, 'No.        Res         Service                  Date                    '
                         'Start                    End                     Status                 '
@@ -52,25 +60,16 @@ def generate_payment_pdf():
             messagebox.showerror("File Not Found", f"CSV file not found for date: {var_date}")
             return
 
+        global novecento_font, armata_font, treb_bold_font, treb_bolditalic_font, treb_regular_font
         pdf = PDF(orientation='P', unit='mm', format='A4')
 
         root_dir = base_dir()
         font_dir = root_dir / "Fonts"
-        pdf.add_font(
-            'Novecento Wide Demi Bold',
-            'B',
-            str(font_dir / 'Novecentowide-Bold.ttf'),
-            uni=True,
-        )
-        pdf.add_font(
-            'Armata Regular',
-            '',
-            str(font_dir / 'Armata-Regular.ttf'),
-            uni=True,
-        )
-        pdf.add_font('Trebuchet MS Bold', 'B', r"C:\Windows\Fonts\trebucbd.ttf", uni=True)
-        pdf.add_font('Trebuchet MS Bold Italic', 'BI', r"C:\Windows\Fonts\trebucbi.ttf", uni=True)
-        pdf.add_font('Trebuchet MS Regular', '', r"C:\Windows\Fonts\trebuc.ttf", uni=True)
+        novecento_font = safe_add_font(pdf, 'Novecento Wide Demi Bold', 'B', font_dir / 'Novecentowide-Bold.ttf')
+        armata_font = safe_add_font(pdf, 'Armata Regular', '', font_dir / 'Armata-Regular.ttf')
+        treb_bold_font = safe_add_font(pdf, 'Trebuchet MS Bold', 'B', r"C:\Windows\Fonts\trebucbd.ttf")
+        treb_bolditalic_font = safe_add_font(pdf, 'Trebuchet MS Bold Italic', 'BI', r"C:\Windows\Fonts\trebucbi.ttf")
+        treb_regular_font = safe_add_font(pdf, 'Trebuchet MS Regular', '', r"C:\Windows\Fonts\trebuc.ttf")
 
         pdf.add_page()
         pdf.alias_nb_pages()
@@ -78,7 +77,7 @@ def generate_payment_pdf():
         page_width = pdf.w - 2 * pdf.l_margin
         th = 5
 
-        pdf.set_font('Trebuchet MS Regular', '', 7)
+        pdf.set_font(treb_regular_font, '', 7)
 
         with open(csv_path, newline='', encoding='utf8') as f:
             reader = csv.reader(f)
@@ -106,7 +105,7 @@ def generate_payment_pdf():
                     pdf.ln(1)
                     pdf.ln(5)
 
-        pdf.set_font('Trebuchet MS Bold Italic', 'BI', 7)
+        pdf.set_font(treb_bolditalic_font, 'BI', 7)
         pdf.ln(5)
         pdf.cell(page_width, 0.0, '***Nothing Follows***', align='C')
 
